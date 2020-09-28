91b97e60c0c88f9078c44d7cc309c4a147bdbe49
Naga Parasa - DE <54954100+nagaparasa@users.noreply.github.com>
2020-09-27 21:11:04 -0500

    adding docker & docker-compose files

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react-with-docker text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/React with Docker APP/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders CI/CD text', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText('For CI/CD setup');
   expect(linkElement).toBeInTheDocument();
 });
